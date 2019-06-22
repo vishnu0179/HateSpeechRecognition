9e2b3c80fb011f39cc2c13a1c90a4180b87bfd5d
vishnu0179 <vjvj140@gmail.com>
2019-06-22 12:05:40 +0530

    Node server added with sample program

---
+++ abc.py
@@ -0,0 +1,8 @@
+import sys
+
+hello = 'hello' + sys.argv[1] + sys.argv[2]
+
+print(hello)
+sys.stdout.flush()
+
+
---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+let spawn = require('child_process').spawn;
+
+
+app.use('/',express.static(__dirname))
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.post('/result', (req,res)=>{
+    let process = spawn('python', ["./abc.py", req.body.first_name, req.body.last_name ])
+
+    process.stdout.on('data', (data)=> {
+        res.send(data.toString())
+    })
+})
+
+app.listen(3000,()=>{
+    console.log('Server started on port 3000')
+})
