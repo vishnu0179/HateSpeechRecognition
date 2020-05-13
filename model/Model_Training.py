ebd49462d85f2ade99dff952dde2214a518cbc3c
aman <aman.r1298@gmail.com>
2020-05-13 19:04:09 +0530

    Properly Indented files

--- model/Model_Training.py
+++ model/Model_Training.py
@@ -1,171 +1,107 @@
+## HOW WE TRAINED THE MODEL
 
-# coding: utf-8
-
-# In[2]:
-
-
-import numpy as np # linear algebra
+import numpy as np 
 import pandas as pd 
 import matplotlib.pyplot as plt
-
-
-# In[31]:
-
-
-train = pd.read_csv(r"C:/users/amanr/desktop/PROJECT/Aman/train_E6oV3lV.csv")
-test = pd.read_csv(r"C:/users/amanr/desktop/PROJECT/aman/test_tweets_anuFYb8.csv")
-train.shape
-
-
-# # 0 not hate 
-# # 1 hate
-
-# In[32]:
-
-
-train
-
-
-# In[33]:
-
-
-train['label'].hist()
-plt.show()
-
-
-# In[34]:
-
-
-train['label'] = train['label'].astype('category')
-
-
-# In[35]:
-
-
-train.info()
-
-
-# In[36]:
-
-
+import pickle
 from nltk.stem import WordNetLemmatizer
 from nltk import tokenize
 from sklearn.feature_extraction.text import TfidfVectorizer
 import re
+from sklearn.model_selection import train_test_split
+#from sklearn.naive_bayes import MultinomialNB
+#from sklearn.ensemble import RandomForestClassifier
+#from sklearn.linear_model import SGDClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+#Importing different models but got best accuracy on LOGISTIC REGRESSION
 
 
-# In[37]:
+
+#LOADING DATA
+train = pd.read_csv(r"../input/train_E6oV3lV.csv")
+test = pd.read_csv(r"../input/test_tweets_anuFYb8.csv")
 
 
+
+#PLOTTING HISTOGRAM
+train['label'].hist()
+plt.show()
+
+
+#TURNING LABELS INTO CATEGORIES
+train['label'] = train['label'].astype('category')
+
+#printing data's information (total values, null values, etc)
+train.info()
+
+
+
+#Removing @, special characters and lemmatizing every word
 train['text_lem'] = [''.join([WordNetLemmatizer().lemmatize(re.sub('[^A-Za-z]',' ',text)) for text in lis]) for lis in train['tweet']]
 test['text_lem'] = [''.join([WordNetLemmatizer().lemmatize(re.sub('[^A-Za-z]',' ',text)) for text in lis]) for lis in test['tweet']]
 
 
-# In[38]:
-
 
 #print(train)#['text_lem'])
-train['text_lem']
-print(train.shape)
+# train['text_lem']
+# print(train.shape)
 
 
-# In[39]:
 
 
-#print(test['text_lem'])
 
 
-# In[40]:
 
 
-from sklearn.model_selection import train_test_split
-
-
+#TRAIN TEST SPLIT
 X_train,X_test,y_train,y_test = train_test_split(train['text_lem'],train['label'])
 
 
-# In[41]:
 
 
-print(y_train)
-      
+#AS the model cant understand words so converting sentences into vector
+#USING TF-IDF vectorizer (Term Freequency - Inverse Document Frequency)
+#another benifit of using TF-IDF vectorizer that is automatically removes wordds which are very common like stopwords,
+#so you dont have to remove stopwords it will work fine !!!
 
-
-# In[42]:
-
-
-print(X_test.shape)
-
-
-# In[43]:
-
-
-print(y_train.shape)
-
-
-# In[44]:
-
-
-#from sklearn.svm import SVC
-from sklearn.linear_model import LogisticRegression
-#from sklearn.naive_bayes import MultinomialNB
-#from sklearn.ensemble import RandomForestClassifier
-#from sklearn.linear_model import SGDClassifier
-from sklearn.metrics import f1_score,accuracy_score
-
-
-# In[45]:
-
-
-
-print(X_train)
-
-
-# In[46]:
-
-
+#Fiting vector on data
 vect = TfidfVectorizer(ngram_range = (1,4)).fit(X_train)
 
 
-# In[47]:
-
-
+#Transforming train and test data based on that data
 vect_transformed_X_train = vect.transform(X_train)
 vect_transformed_X_test = vect.transform(X_test)
 
 
-# In[48]:
 
-
-#print(vect.get_feature_names())
+#Printing shapes
 print(vect_transformed_X_train.shape)
 print(vect_transformed_X_test.shape)
 print(y_train.shape)
 print(y_test.shape)
 
 
-# In[1]:
 
-
+#Using Logistic Regression
 modelLR = LogisticRegression(C=100).fit(vect_transformed_X_train,y_train)
 predictionsLR = modelLR.predict(vect_transformed_X_test)
 
-
-print(sum(predictionsLR==1),len(y_test),f1_score(y_test,predictionsLR))
+#Printing Accuracy
 print('Accuracy :',accuracy_score(y_test,predictionsLR))
-# print(y_test.shape)
 
 
-# In[50]:
 
-
+#printing Predictions
 print(predictionsLR)
 
 
-# In[ ]:
 
 
-# import pickle
+
+#TO save the model and vector for future use on other systems
+
+
 
 # with open('model2','wb') as f:
 #     pickle.dump(modelLR,f)
