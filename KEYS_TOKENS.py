dfa13136cf7b10410c2ca2d508859f261c020688
vishnu0179 <vjvj140@gmail.com>
2020-05-22 23:46:53 +0530

    removed tweepy access tokens from keytoktens.py

--- KEYS_TOKENS.py
+++ KEYS_TOKENS.py
@@ -1,9 +1,9 @@
 #keys and tokens for authentication
 
-CONSUMER_KEY='9AN9fdaTQwiU2Bz9odY6gf1Zg'
-CONSUMER_SECRET ='Net0KwognLHex4Gk0qQ6QZH4MHzMDw5EBnaLDCclAcYoJZKX19'
-ACCESS_TOKEN ='1136507147326377984-oVIAqUW5lubEqfSlzKR4EYbK0qkamB'
-ACCESS_TOKEN_SECRET ='NfLxed2ldlX8s2Uq0eg3S913Rq802vQcIyNzFetPNhGQy'
+CONSUMER_KEY=''
+CONSUMER_SECRET =''
+ACCESS_TOKEN =''
+ACCESS_TOKEN_SECRET =''
 
 
 # Please add your own access tokens and keys here.
