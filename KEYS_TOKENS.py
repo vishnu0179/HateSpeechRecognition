7b7c5c07f5273ea25d6fcfbc7eb677a25a7cbb28
vishnu0179 <vjvj140@gmail.com>
2020-05-22 23:51:58 +0530

    added the tokens back

--- KEYS_TOKENS.py
+++ KEYS_TOKENS.py
@@ -1,9 +1,10 @@
 #keys and tokens for authentication
 
-CONSUMER_KEY=''
-CONSUMER_SECRET =''
-ACCESS_TOKEN =''
-ACCESS_TOKEN_SECRET =''
+CONSUMER_KEY='9AN9fdaTQwiU2Bz9odY6gf1Zg'
+CONSUMER_SECRET ='Net0KwognLHex4Gk0qQ6QZH4MHzMDw5EBnaLDCclAcYoJZKX19'
+ACCESS_TOKEN ='1136507147326377984-oVIAqUW5lubEqfSlzKR4EYbK0qkamB'
+ACCESS_TOKEN_SECRET ='NfLxed2ldlX8s2Uq0eg3S913Rq802vQcIyNzFetPNhGQy'
+
 
 
 # Please add your own access tokens and keys here.
